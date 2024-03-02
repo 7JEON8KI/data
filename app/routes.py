b9c61742f569f4d8d6f13c90ec1fd6bf818478e0
SoMin <thals0107@naver.com>
2024-03-02 14:20:55 +0900

    feat: 이미지 검색 기능 추가

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile
 from pydantic import BaseModel
+from fastapi.routing import APIRouter
+import torchvision.transforms as transforms
+from pydantic import BaseModel
+from PIL import Image
+from io import BytesIO
 
 from .model.HybridModel import hybrid_recommender
+
+from .model.ImageSearch import image_processing_and_search
 
 router = APIRouter(prefix="/ai", tags=["ai"])
 
@@ -12,9 +19,19 @@ class ProductItem(BaseModel):
     productName: str
     productMainImage: str
 
+# class SearchImage(BaseModel):
+#     productImage: UploadFile
+
 @router.post("/hybrid-recommendations")
 async def calculate_recommendations_scores(item: ProductItem):
     target_product = item.productName
     target_image_path = item.productMainImage
     hybrid_recommendations = hybrid_recommender(target_product, target_image_path)
     return hybrid_recommendations
+
+@router.post("/image-search")
+async def image_search(file: UploadFile):
+    contents = await file.read()
+    img = Image.open(BytesIO(contents))
+    image_search_results = image_processing_and_search(img)
+    return image_search_results
